c7aecac5f150c9cab4740a54d9705187dfffe7a3
hori <fourbasic19@gmail.com>
2023-05-19 11:38:02 +0900

    【CORS】CORSをパッケージシステムを利用して管理

--- app.js
+++ app.js
@@ -5,23 +5,16 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const cors = require("cors");
 
 const User = require("./models/User");
 app.use(express.json());
 const port = 5000;
 
 // CORSを許可する設定
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
 
-//データベースと接続
+// データベースと接続
 const start = async () => {
   try {
     await connectDB(process.env.DB_URI);
@@ -35,11 +28,11 @@ const start = async () => {
 
 start();
 
-//ルーティング設計(task)
+// ルーティング設計 (task)
 app.use("/api/v1/tasks", taskRoute);
 
 // ユーザーの全件取得
 app.use("/api/v1/users", userRoute);
 
-//ユーザー新規登録API
+// ユーザー新規登録API
 app.use("/api/v1", authRoute);
