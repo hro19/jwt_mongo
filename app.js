6f94d9822f2189af440fe5497e5d0941d04dc9d8
hro19 <fourbasic19@gmail.com>
2024-01-30 02:10:00 +0900

    【cors】オリジン設定

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ app.use(express.json());
 const port = 5000;
 
 // CORSを許可する設定
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://jwt-front.pages.dev"],
+  credentials: true
+}));
 
 // データベースと接続
 const start = async () => {
